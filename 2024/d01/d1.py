5e2e41de4a16e3ca607be450d9c97ac7d81954e1
Erik Zakariassen <erikzak@gmail.com>
2024-12-02 22:28:48 +0100

    solved 2024 d1

--- 2024/d01/d1.py
+++ 2024/d01/d1.py
@@ -2,21 +2,37 @@
 import sys
 
 
-def solve_part1(input: str) -> int:
+def solve_part1(puzzle: str) -> int:
     """Solution part 1."""
+    left, right = [], []
+    for line in puzzle:
+        split = line.strip().split()
+        left.append(int(split[0]))
+        right.append(int(split[1]))
+    distance = sum(abs(l-r) for l, r in zip(sorted(left), sorted(right)))
+    return distance
 
 
-def solve_part2(input: str) -> int:
+def solve_part2(puzzle: str) -> int:
     """Solution part 2."""
+    numbers, counts = {}, {}
+    for line in puzzle:
+        left, right = (int(v) for v in line.strip().split())
+        numbers[left] = numbers.get(left, 0) + 1
+        counts[right] = counts.get(right, 0) + 1
+    similarity = sum(number * counts.get(number, 0) * numbers[number] for number in numbers)
+    return similarity
 
 
 def main():
     """Runs puzzle solutions."""
-    dev_flag = any(arg in ["-d", "--dev"] for arg in sys.argv)
-    input_file = "test.txt" if (dev_flag) else "input.txt"
-
+    test = any(arg in ["-t", "--test"] for arg in sys.argv)
+    input_file = "test.txt" if (test) else "input.txt"
     with open(input_file, "r", encoding="utf-8") as reader:
-        puzzle_input = reader.read().strip()
-
+        puzzle_input = reader.readlines()
     print(solve_part1(puzzle_input))
     print(solve_part2(puzzle_input))
+
+
+if (__name__ == "__main__"):
+    main()
--- 2024/template.py
+++ 2024/template.py
@@ -2,21 +2,23 @@
 import sys
 
 
-def solve_part1(input: str) -> int:
+def solve_part1(puzzle: str) -> int:
     """Solution part 1."""
 
 
-def solve_part2(input: str) -> int:
+def solve_part2(puzzle: str) -> int:
     """Solution part 2."""
 
 
 def main():
     """Runs puzzle solutions."""
-    dev_flag = any(arg in ["-d", "--dev"] for arg in sys.argv)
-    input_file = "test.txt" if (dev_flag) else "input.txt"
-
+    test = any(arg in ["-t", "--test"] for arg in sys.argv)
+    input_file = "test.txt" if (test) else "input.txt"
     with open(input_file, "r", encoding="utf-8") as reader:
-        puzzle_input = reader.read().strip()
-
+        puzzle_input = reader.readlines()
     print(solve_part1(puzzle_input))
     print(solve_part2(puzzle_input))
+
+
+if (__name__ == "__main__"):
+    main()
